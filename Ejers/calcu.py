48db7b820a0007e25dc9c125a7d5bce1fa9db146
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-03-23 17:57:14 -0300

    TP1 finished

--- Ejers/calcu.py
+++ Ejers/calcu.py
@@ -1,14 +1,42 @@
 ## Calculadora con getopt 
 
-from ast import arg
 import sys
 import getopt
 
 def calculate():
-    argv = sys.argv[1:]
+    operation = None
+    n = None
+    m = None
+    total = None
 
-    opts,args = getopt.getopt(argv, "-o:-n:-m")
+    argv = sys.argv[1:]
+    #printea argumentos
+
+    opts, args = getopt.getopt(argv, 'o:n:m:')
+    # print("Opciones: ", opt)
+    # print("Argumentos: ",arg)
+
 
     for opt, arg in opts:
         if opt in ['-o']:
-            
+            operation = arg
+        elif opt in ['-n']:
+            n = int(arg)
+        elif opt in ['-m']:
+            m = int(arg)
+
+    if operation == "*":
+        total = n*m
+    elif operation == "+":
+        total = n+m
+    elif operation == "-":
+        total = n-m
+    elif operation == "/":
+        total = n/m
+    print('operation:{}'.format(operation))
+    print('n:{}'.format(n))
+    print('m:{}'.format(m))
+
+    print('El total es {}'.format(total))
+
+calculate()
