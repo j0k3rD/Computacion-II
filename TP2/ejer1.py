683c9333608518b2b92c8735e047ac7e8e936f7f
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-03-25 14:24:35 -0300

    almost finished

--- TP2/ejer1.py
+++ TP2/ejer1.py
@@ -6,7 +6,9 @@ import argparse
 import os
 import sys
 import subprocess as sp
+import time
 
+now = time.strftime("%c")
 
 
 def main():
@@ -15,25 +17,30 @@ def main():
     parser.add_argument('-f',type=str,default=1,help="Pase un archivo como argumento. O simplemente el nombre para crear uno")
     parser.add_argument('-l',type=str,default=1,help="Pase un archivo como argumento. O simplemente el nombre para crear uno")
     args = parser.parse_args()
-    func_arch(args)
-#    sys.stdout.write(str(func_arch(args)))
+    sys.stdout.write(str(func_exist(args)))
+    sys.stdout.write(str(func_arch(args)))
 
-# def func_exist(args):
-#     if os.path.exists(args.f) == False:
-#         args.f = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.f), "w")
-#         return "Archivo f creado con exito"
-#     elif os.path.exists(args.l) == False:
-#         args.l = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w")
-#         return "Archivo l creado con exito"
-#     else:
-#         return "Argumentos invalidos"
+
+
+def func_exist(args):
+    if os.path.exists(args.f) == False:
+        args.f = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.f), "w")
+        return "Archivo f creado con exito"
+    elif os.path.exists(args.l) == False:
+        args.l = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w")
+        return "Archivo l creado con exito"
+    else:
+        return "Argumentos invalidos"
+
+
 
 def func_arch(args):
     fd = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.f), "w+")
     sp.Popen(["{}".format(args.c)], stdout=fd)
 
-    fd = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w+")
-    sp.Popen(["{}:, Comando: ,{} ejecutado con exito".format(date, args.c)], stdout=fd, stderr=fd)
+    fs = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w+")
+    sp.Popen(["{}:, Comando: ,{} ejecutado con exito".format(now, args.c)], stdout=fs)
+    # sp.Popen(["{}:, Comando: ,{} ejecutado con exito".format(now, args.c)], stderr=fd)
 
 if __name__=='__main__':
     main()
