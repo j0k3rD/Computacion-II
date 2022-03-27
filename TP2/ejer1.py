d59b59c13ee3b467107259c106e7b8f41bf0251c
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-03-27 15:35:56 -0300

    tp2 almost finished, 1 error

--- TP2/ejer1.py
+++ TP2/ejer1.py
@@ -3,10 +3,12 @@
 ## Ejercitacion con subprocess.Popen [popen]
 
 import argparse
+from asyncio import subprocess
+from asyncio.subprocess import STDOUT
 from concurrent.futures import process
 import os
 import sys
-import subprocess as sp
+from subprocess import Popen, PIPE, STDOUT
 import time
 
 now = time.strftime("%c")
@@ -18,35 +20,33 @@ def main():
     parser.add_argument('-f',type=str,help="Pase un archivo como argumento. O simplemente el nombre para crear uno")
     parser.add_argument('-l',type=str,help="Pase un archivo como argumento. O simplemente el nombre para crear uno")
     args = parser.parse_args()
-    # sys.stdout.write(str(func_exist(args)))
-    sys.stdout.write(str(func_arch(args)))
+    func_arch(args)
+    func_arch(args)
 
 
-
-
-
-# def func_exist(args):
-#     if os.path.exists(args.f) == False:
-#         args.f = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.f), "w")
-#         return "Archivo f creado con exito"
-#     elif os.path.exists(args.l) == False:
-#         args.l = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w")
-#         return "Archivo l creado con exito"
-#     else:
-#         return "Argumentos invalidos"
+def func_exist(args):
+    if os.path.exists(args.f) == False:
+        args.f = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.f), "w")
+        return "Archivo f creado con exito"
+    elif os.path.exists(args.l) == False:
+        args.l = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w")
+        return "Archivo l creado con exito"
+    else:
+        return "Argumentos invalidos"
 
 
 def func_arch(args):
     fs = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.f), "w+")
-    sp.Popen(["{}".format(args.c)], stdout=fs)
+    Popen(["{}".format(args.c)],shell=True, stdout=fs)
 
     fd = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w+")
-    # sp.Popen(["{}".format(args.c)], stdout=fd)
-    sp.Popen(["{}".format(args.c)], stdout=sp.PIPE, stderr=sp.PIPE)
-    # sp.Popen(["{}:, Comando: ,{} ejecutado con exito".format(now, args.c)], stderr=fd)
+    p = Popen(["{}".format(args.c)], shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    output = p.stdout.read()
+    with open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w+") as error_file:
+        error_file.write(str(output))
+ 
 
 if __name__=='__main__':
     main()
 
 
-    
---
+++ TP2/ex.py
@@ -0,0 +1,6 @@
+from ast import Return
+from subprocess import Popen, PIPE, STDOUT
+
+cmd = 'ls /etc/fstab /etc/non-existent-file'
+p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+output = p.stdout.read()
