5223dc6715a5aacc945686437cdafe59a320bf7c
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-03-27 17:10:53 -0300

    tp2 finished

--- TP2/ejer1.py
+++ TP2/ejer1.py
@@ -7,6 +7,7 @@ from asyncio import subprocess
 from asyncio.subprocess import STDOUT
 from concurrent.futures import process
 import os
+from re import A
 import sys
 from subprocess import Popen, PIPE, STDOUT
 import time
@@ -20,33 +21,50 @@ def main():
     parser.add_argument('-f',type=str,help="Pase un archivo como argumento. O simplemente el nombre para crear uno")
     parser.add_argument('-l',type=str,help="Pase un archivo como argumento. O simplemente el nombre para crear uno")
     args = parser.parse_args()
+    func_exist(args)
     func_arch(args)
-    func_arch(args)
-
+    
 
 def func_exist(args):
-    if os.path.exists(args.f) == False:
-        args.f = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.f), "w")
-        return "Archivo f creado con exito"
-    elif os.path.exists(args.l) == False:
-        args.l = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w")
-        return "Archivo l creado con exito"
+
+    if os.path.exists(args.f):
+        file_f = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.f), "r+")
     else:
-        return "Argumentos invalidos"
+        file_f = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.f), "w")
+        return "Archivo f creado con exito"
+    
+    if os.path.exists(args.l):
+        file_l = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "r+")
+    else:
+        file_l = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w")
+        return "Archivo f creado con exito"
 
 
 def func_arch(args):
-    fs = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.f), "w+")
-    Popen(["{}".format(args.c)],shell=True, stdout=fs)
 
-    fd = open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w+")
-    p = Popen(["{}".format(args.c)], shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-    output = p.stdout.read()
-    with open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w+") as error_file:
-        error_file.write(str(output))
+    p = Popen(["{}".format(args.c)], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+
+
+    if p.returncode == 0:
+        with open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.f), "w+") as fd:
+            fd.write(str(out))
+
+        cmd = str(args.c)
+        txt = "Fecha y Hora: '{}'. Comando: '{}' ejecutado correctamente.".format(now, cmd)
+        with open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w+") as error_file:
+            error_file.write(str(txt))
+    else:
+        with open("/home/aaron/Documents/Computacion II/TPS/TP2/{}.txt".format(args.l), "w+") as error_file:
+            error_file.write(str(err))
  
 
 if __name__=='__main__':
     main()
 
 
+## Bibliografias usadas: https://www.saltycrane.com/blog/2008/09/how-get-stdout-and-stderr-using-python-subprocess-module/
+#  Tenia un problema ya que el stderr no me lo estaba printeando bien y ademas cuando se pasaba un comando bien tambien pasaba
+# como error. Este video ayudo a entender el PIPE y comunicate() para generar las dos salidas.
+# https://www.youtube.com/watch?v=VlfLqG_qjx0
+                        
