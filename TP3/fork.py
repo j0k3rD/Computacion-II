768cfc2c5aec3e212fcd575a46ce959d189f5af9
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-04-03 16:13:26 -0300

    almost finished tp3

--- TP3/fork.py
+++ TP3/fork.py
@@ -18,23 +18,32 @@ def main():
     # parser.add_argument('-h',type=str,help="Ayuda.")
     parser.add_argument('-v',type=str,help="Modo Verboso.")
     args = parser.parse_args()
+
     if args.v == None:
-        print("Ejecucion SIN Modo Verboso")
+        print("Ejecucion SIN Modo Verboso\n")
         fork_F(args)
-        # p_son(args)
     else:
-        print("Ejecucion CON Modo Verboso")
+        print("Ejecucion CON Modo Verboso\n")
         verbose(args)
-        # p_son(args)
+    # elif args.h == True and args.v == None:
+    #     help_F()
+    #     print("Ejecucion SIN Modo Verboso")
+    #     verbose(args)
+    # elif args.h == None and args.v == True:
+    #     help_F()
+    #     print("Ejecucion CON Modo Verboso")
+    #     verbose(args)
+    # elif args.h == True and args.v == True:
+    #     help_F()
+    #     print("Ejecucion CON Modo Verboso")
+    #     verbose(args)
 
 
 def fork_F(args):        
-        # for i in range(0, args.n):
+    for i in range(args.n):
         ret = os.fork()
         if ret == 0:
             # print(subprocess.check_output(["ps", "fax"], universal_newlines=True))
-            # os.wait()
-            # time.sleep(1)
             suma = 0
             i = 0
             while i <= os.getpid():
@@ -42,16 +51,50 @@ def fork_F(args):
                     suma = suma+i
                 i+=1
             print("PID: ", os.getpid(), "-", "PPID: ", os.getppid(), ":", suma)
+            # time.sleep(2)
+            os._exit(0)
         else:
             time.sleep(0.0001)
-            print("Soy el padre")
             os.wait()
 
 
 def verbose(args):
     STARTING = "Starting process"
-    FINISHING = "Ending process"
+    ENDING = "Ending process"
+    SUM = None
     
+
+    for i in range(args.n):
+        print(STARTING, os.getpid())
+        ret = os.fork()
+        # for i in range(0, args.n):
+        if ret == 0:
+            # print(STARTING, os.getpid())
+            # print(subprocess.check_output(["ps", "fax"], universal_newlines=True))
+            suma = 0
+            i = 0
+            while i <= os.getpid():
+                if i % 2 == 0:
+                    suma = suma+i
+                i+=1
+            # time.sleep(2)
+            print(ENDING, os.getpid())
+            print("PID: ", os.getpid(), "-", "PPID: ", os.getppid(), ":", suma)
+            os._exit(0)
+
+        else:
+            time.sleep(0.0001)
+            os.wait()
+
+
+def help_F(args):
+    INIT_TXT= """Como usar:"
+                            1. Escriba en su terminal 'python3'.
+                            2. Seguido de '-n' donde debe poner la cantidad de procesos hijos a crear.
+                            3. Luego puede elegir si agregar el Modo Verboso, escribiendo '-v', el cual mostrará detalles
+                            sobre como se va ejecutando el proceso.
+                            4. Por ultimo de 'Enter' para comenzar la ejecucion.
+                            """
 
 
 if __name__=='__main__':
