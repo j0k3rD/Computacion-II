a023f8e19d944cf2e8b4f10938cec3bfb758b9ef
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-04-03 18:24:28 -0300

    endend tp3

--- TP3/fork.py
+++ TP3/fork.py
@@ -5,38 +5,37 @@
 #----------------------------------#
 
 import argparse
+from curses.ascii import EM
 from multiprocessing.connection import wait
 import os
+from queue import Empty
 import subprocess
 from time import sleep
 import time
+from tokenize import group
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-n',type=int,help="Numero de Forks a realizar (n de proc hijos). EJ: '3'")
-    # parser.add_argument('-h',type=str,help="Ayuda.")
-    parser.add_argument('-v',type=str,help="Modo Verboso.")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-he', action='store_true', help="Activa: Ayuda.")
+    group.add_argument('-v', action='store_true', help="Activa: Modo Verboso.")
     args = parser.parse_args()
 
-    if args.v == None:
-        print("Ejecucion SIN Modo Verboso\n")
-        fork_F(args)
+    if args.he:
+        print("AYUDA: \n")
+        help_F(args)
     else:
+        print("Ejecutado SIN ayuda.\n")
+
+    if args.v:
         print("Ejecucion CON Modo Verboso\n")
         verbose(args)
-    # elif args.h == True and args.v == None:
-    #     help_F()
-    #     print("Ejecucion SIN Modo Verboso")
-    #     verbose(args)
-    # elif args.h == None and args.v == True:
-    #     help_F()
-    #     print("Ejecucion CON Modo Verboso")
-    #     verbose(args)
-    # elif args.h == True and args.v == True:
-    #     help_F()
-    #     print("Ejecucion CON Modo Verboso")
-    #     verbose(args)
+    else:
+        print("Ejecucion SIN Modo Verboso\n")
+        fork_F(args)
+
 
 
 def fork_F(args):        
@@ -61,15 +60,13 @@ def fork_F(args):
 def verbose(args):
     STARTING = "Starting process"
     ENDING = "Ending process"
-    SUM = None
     
 
     for i in range(args.n):
-        print(STARTING, os.getpid())
         ret = os.fork()
         # for i in range(0, args.n):
         if ret == 0:
-            # print(STARTING, os.getpid())
+            print(STARTING, os.getpid())
             # print(subprocess.check_output(["ps", "fax"], universal_newlines=True))
             suma = 0
             i = 0
@@ -81,20 +78,20 @@ def verbose(args):
             print(ENDING, os.getpid())
             print("PID: ", os.getpid(), "-", "PPID: ", os.getppid(), ":", suma)
             os._exit(0)
-
         else:
             time.sleep(0.0001)
             os.wait()
 
 
 def help_F(args):
-    INIT_TXT= """Como usar:"
+    INIT_TXT= """Como usar:
                             1. Escriba en su terminal 'python3'.
                             2. Seguido de '-n' donde debe poner la cantidad de procesos hijos a crear.
                             3. Luego puede elegir si agregar el Modo Verboso, escribiendo '-v', el cual mostrará detalles
                             sobre como se va ejecutando el proceso.
                             4. Por ultimo de 'Enter' para comenzar la ejecucion.
                             """
+    print(INIT_TXT)
 
 
 if __name__=='__main__':
