6f0a368baf74ed2080ff98aa8f011c2d030658c4
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-04-02 21:20:22 -0300

    start tp3

--- TP3/fork.py
+++ TP3/fork.py
@@ -1,47 +1,53 @@
-## Ejercicio del TP3 - Uso de Fork()
+## Ejercicio del TP3 - Uso de Fork() - SUMA DE PROCESOS PARES
 
 # Fork es un metodo de python que sirve para crear procesos hijos de otros.
 
 #----------------------------------#
 
 import argparse
+from multiprocessing.connection import wait
 import os
 
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n',type=str,help="Numero de Forks a realizar (n de proc hijos). EJ: '3'")
+    parser.add_argument('-n',type=int,help="Numero de Forks a realizar (n de proc hijos). EJ: '3'")
     # parser.add_argument('-h',type=str,help="Ayuda.")
     parser.add_argument('-v',type=str,help="Modo Verboso.")
     args = parser.parse_args()
-    fork_F()
-    p_son()
+    print(args.n)
+    if args.v == None:
+        print("Ejecucion SIN Modo Verboso")
+        fork_F(args)
+        # p_son(args)
+    else:
+        print("Ejecucion CON Modo Verboso")
+        verbose(args)
+        # p_son(args)
 
 
 def fork_F(args):
-    for i in args.n:
-        ret = os.fork()
-        if ret == 0:
-            p_son()            
-
-
-def p_son(args):
-    sum=0
-    i = 0
+    for i in range(args.n):
+        os.fork()
+        # ret = os.fork()
+        # if ret == 0:
+        #     p_son()            
+# def p_son(args):
+    suma = 0
     while i <= os.getpid():
         if i % 2 == 0:
-            print(i)
-            suma = suma+1
+            suma = suma+i
         i+=1
+            # for i in range(args.n):
+            #     os.wait()
     print("PID: ", os.getpid(), "-", "PPID: ", os.getppid(), ":", suma)
 
 
-
-
-
-
-
+def verbose(args):
+    STARTING = "Starting process"
+    FINISHING = "Ending process"
+    
 
 
 if __name__=='__main__':
