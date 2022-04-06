ccd9abc9ed9cfe1b5e79ec4464f94014b7f704fd
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-04-06 10:30:18 -0300

    tp3 fork, add for, wait()

--- TP3/fork.py
+++ TP3/fork.py
@@ -20,6 +20,8 @@ def main():
     parser.add_argument('-n',type=int,help="Numero de Forks a realizar (n de proc hijos). EJ: '3'")
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-he', action='store_true', help="Activa: Ayuda.")
+    #EN este caso se creo una ayuda personalizada para ayudar al usuario como usar el sript, pero argsparse ya trae
+    #su propia ayuda con -h que el es help que ponemos en ellos.
     group.add_argument('-v', action='store_true', help="Activa: Modo Verboso.")
     args = parser.parse_args()
 
@@ -38,7 +40,6 @@ def main():
         print("Ejecucion SIN Modo Verboso\n")
 
 
-
 def fork_F(args):        
     for i in range(args.n):
         ret = os.fork()
@@ -50,7 +51,18 @@ def fork_F(args):
             os._exit(0)
         else:
             time.sleep(0.0001)
-            os.wait()
+            #Hacerlo de esta forma no tiene sentido ya que por cada hijo que creo lo voy a esperar hasta que termine.
+            #No aprovecha el multiprocesing.
+            #Por lo que lo que estaria bien, pero no del todo. Lo que habria que hacer es hacer un for i in range afuera
+            #del for este para que haga un wait por cada uno de los procesos. Los crea todos de una y despues los
+            #mata tambien.
+            # MAL
+         ## os.wait()
+            # MAL
+
+    for i in range(args.n):
+        wait()
+    #Ese for seria la opcion correcta.
 
 
 def operation(args):
@@ -77,8 +89,8 @@ def verbose(args):
             print(ENDING, os.getpid())    
             os._exit(0)          
     time.sleep(3)
-    os.wait()
-
+    for i in range(args.n):
+        wait()
 
 
 def help_F(args):
