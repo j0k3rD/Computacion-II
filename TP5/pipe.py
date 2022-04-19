b8159e08124eac8ad2c5159c5a5b79a9689b9760
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-04-19 08:29:39 -0300

    tp pipes started, functions and file

---
+++ TP5/pipe.py
@@ -0,0 +1,31 @@
+##  Ejercicio del TP5 - Uso de Pipes - Inversor de texto
+
+# Las "pipes" sirven para poder redireccionar una salida de un proceso hacia la entrada de otro y asi comunicarlos.
+
+import argparse
+import constants
+import os
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f',type=str,help="Ingrese el nombre del archivo de texto a usar. EJ: 'test'")
+    args = parser.parse_args()
+    func_exist(args)
+
+
+def func_exist(args):
+    if os.path.exists(args.f):
+        file_f = open("{}{}.txt".format(constants.FILE, args.f), "r")
+        return "Archivo f creado con exito"
+    else:
+        return "Archivo NO ENCONTRADO!"
+
+
+def invest(args):
+    with open(constants.FILE) as fd:
+        lines = sum(1 for line in fd)
+        print(lines)
+    for i in range(lines):
+        ret = os.fork()
+        if ret == 0:
+
