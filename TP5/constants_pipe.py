f0fe5303fd0456041a70e7abb51d729131c18640
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-04-19 14:26:12 -0300

    tp pipe ended

--- TP5/constants_pipe.py
+++ TP5/constants_pipe.py
@@ -1,3 +1,8 @@
 # Constantes del TP5 de Pipes.
 
 FILE = "/home/aaron/Documents/Computacion II/TPS/TP5/"
+
+TEXT = """Hola Mundo
+            que tal
+            este es un archivo
+            de ejemplo"""
--- TP5/pipe.py
+++ TP5/pipe.py
@@ -12,23 +12,28 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f',type=str,help="Ingrese el nombre del archivo de texto a usar. EJ: 'test.txt'")
     args = parser.parse_args()
-    func_exist(args)
+    # func_exist(args)
     invest(args)
 
-def func_exist(args):
-    if os.path.exists(args.f):
-        file_f = open("{}{}".format(constants_pipe.FILE, args.f), "r")
-        return "Archivo f creado con exito"
-    else:
-        return "Archivo NO ENCONTRADO!"
+# def func_exist(args):
+#     if os.path.exists(args.f):
+#         file_f = open("{}{}".format(constants_pipe.FILE, args.f), "r")
+#         return "Archivo f creado con exito"
+#     else:
+#         return "Archivo NO ENCONTRADO!"
 
 
 def invest(args):
-    with open("test.txt") as fd:
-        lines = sum(1 for line in fd)
-        print(lines)
+    with open(args.f, 'w') as fd:
+        fd.write(constants_pipe.TEXT)
+    fs = open(args.f,'r')
+    lines = fs.readlines()
+    fs.close()
 
-    for i in range(lines):
+        # lines = sum(1 for line in fd)
+        # print(lines)
+
+    for i in range(len(lines)):
         #Inicio el descriptor r,w
         r,w = os.pipe()
         ret = os.fork()
@@ -36,23 +41,24 @@ def invest(args):
             #Proceso que hacen los hijos
             os.close(w)
             r = os.fdopen(r,'r')
-            print("Child reading")
-            line = fd.readline()
-            print("{}[::-1].strip()".format(line))
+            # print("Child reading")
+            line = r.readline()
+        # No puedo poner solo el parametro de lines dentro de las llaves y agregarle lo otro ya que todo hace
+        # una sola funcion por lo que lo hacemos con la "f" al principio en vez de format.
+            print(f"{line[::-1].strip()}")
             r.close()
-            print("Child closing")
+            # print("Child closing")
 
         else:
             #Proceso que realiza el Padre
             os.close(r)
             w = os.fdopen(w,'w')
-            print("Father writting")
+            # print("Father writting")
             w.write(lines[i])
             w.close()
-            print("Father closing")
+            # print("Father closing")
             os.wait()
             sys.exit(0)
-
 
 
 if __name__=='__main__':
