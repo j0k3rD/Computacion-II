0c8c38040800f421134d4924169ee2f533ce641c
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-04-19 08:40:08 -0300

    pipe started, r,w and fork

--- TP5/pipe.py
+++ TP5/pipe.py
@@ -8,7 +8,7 @@ import os
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f',type=str,help="Ingrese el nombre del archivo de texto a usar. EJ: 'test'")
+    parser.add_argument('-f',type=str,help="Ingrese el nombre del archivo de texto a usar. EJ: 'test.txt'")
     args = parser.parse_args()
     func_exist(args)
 
@@ -26,6 +26,7 @@ def invest(args):
         lines = sum(1 for line in fd)
         print(lines)
     for i in range(lines):
+        r,w = os.pipe()
         ret = os.fork()
         if ret == 0:
 
