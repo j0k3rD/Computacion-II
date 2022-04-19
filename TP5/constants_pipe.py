10487d6e872f0eb7e876111eb341596c61a91350
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-04-19 14:06:35 -0300

    almost ending, error file

---
+++ TP5/constants_pipe.py
@@ -0,0 +1,3 @@
+# Constantes del TP5 de Pipes.
+
+FILE = "/home/aaron/Documents/Computacion II/TPS/TP5/"
--- TP5/pipe.py
+++ TP5/pipe.py
@@ -3,30 +3,57 @@
 # Las "pipes" sirven para poder redireccionar una salida de un proceso hacia la entrada de otro y asi comunicarlos.
 
 import argparse
-import constants
+import constants_pipe
 import os
+import sys
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f',type=str,help="Ingrese el nombre del archivo de texto a usar. EJ: 'test.txt'")
     args = parser.parse_args()
     func_exist(args)
-
+    invest(args)
 
 def func_exist(args):
     if os.path.exists(args.f):
-        file_f = open("{}{}.txt".format(constants.FILE, args.f), "r")
+        file_f = open("{}{}".format(constants_pipe.FILE, args.f), "r")
         return "Archivo f creado con exito"
     else:
         return "Archivo NO ENCONTRADO!"
 
 
 def invest(args):
-    with open(constants.FILE) as fd:
+    with open("test.txt") as fd:
         lines = sum(1 for line in fd)
         print(lines)
+
     for i in range(lines):
+        #Inicio el descriptor r,w
         r,w = os.pipe()
         ret = os.fork()
         if ret == 0:
+            #Proceso que hacen los hijos
+            os.close(w)
+            r = os.fdopen(r,'r')
+            print("Child reading")
+            line = fd.readline()
+            print("{}[::-1].strip()".format(line))
+            r.close()
+            print("Child closing")
 
+        else:
+            #Proceso que realiza el Padre
+            os.close(r)
+            w = os.fdopen(w,'w')
+            print("Father writting")
+            w.write(lines[i])
+            w.close()
+            print("Father closing")
+            os.wait()
+            sys.exit(0)
+
+
+
+if __name__=='__main__':
+    main()
