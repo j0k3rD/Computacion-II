fc45c1fe3dfe04c47f345dc5e6dda2d277475e72
Aaron <agm.moya@alumno.um.edu.ar>
2022-05-04 08:56:11 -0300

    fork problem

--- TP6/mapping.py
+++ TP6/mapping.py
@@ -8,19 +8,22 @@ import sys
 import mmap
 import signal
 
-pid_H1 = None
-pid_H2 = None
+# pid_H2 = 0
+
 
 def handler_father(s,f):
-    #Señales para matar a los hijos
-    if s == signal.SIGUSR1:
-        os.kill(pid_H2, signal.SIGUSR1)
-    elif s == signal.SIGUSR2:
-        os.kill(pid_H2, signal.SIGUSR2)
-        print("Father exiting..")
-        for i in range(2):
-            os.wait()
-        sys.exit(0)
+    # if pid_H2 == 0:
+    #     pass
+    # else:
+        if s == signal.SIGUSR1:
+            os.kill(pid_H2, signal.SIGUSR1)
+        elif s == signal.SIGUSR2:
+            os.kill(pid_H2, signal.SIGUSR2)
+            #Para finalizar los dos hijos
+            for i in range(2):
+                os.wait()
+            print("Father exiting..")
+            sys.exit(0)
 
 
 def handler_H2(s,f):
@@ -48,44 +51,63 @@ fd = os.open("{}test.txt".format(args.f), os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
 
 print("Starting..")
 
-for i in range(2):
+# for i in range(2):
+try:
     pid = os.fork()
     if pid == 0:
-        if i == 0:
-            #Procesos que realiza el HIJO 1
-            pid_H1 = os.getpid()
-            for line in sys.stdin:
-                if line == "bye":
-                    os.kill(pid_H1, signal.SIGUSR2)
-                    print("H1 Exiting")
-                    sys.exit(0)
+    # if not os.fork():
+        #Procesos que realiza el HIJO 1
+        # if i == 0:
+            # pid_list.append(os.getpid())
+        for line in sys.stdin:
+            if line[:3] == "bye":
+                os.kill(os.getppid(), signal.SIGUSR2)
+                print("H1 Exiting")
+                sys.exit(0)
             memory.write(line.encode())
-            os.kill(pid_H1,signal.SIGUSR1)
+            os.kill(os.getppid(),signal.SIGUSR1)
+        # pid_H2 = os.fork()
+        #         if not pid_H2:
+except OSError:
+    print("Fork H1 failed")
 
-        else:
-            #Proceso que realiza el HIJO 2
-            pid_H2 = os.getpid()
-            signal.signal(signal.SIGUSR1, handler_H2)
-            signal.signal(signal.SIGUSR2, handler_H2)
-            while True:
-                signal.pause()
-
-    else:
-        #Procesos que realiza el PADRE
-        print("Father Waiting...")
-        signal.signal(signal.SIGUSR1, handler_father)
-        signal.signal(signal.SIGUSR2, handler_father)
-
+try:
+    # else:
+        #Proceso que realiza el HIJO 2
+    pid_H2 = os.fork() 
+    if pid_H2 == 0:
+    # pid_H2 = os.getpid()
+        signal.signal(signal.SIGUSR1, handler_H2)
+        signal.signal(signal.SIGUSR2, handler_H2)
         while True:
             signal.pause()
-            print("Father Reading: ", memory.readline().decode())
-            print("Father Notifying..")
+except OSError:
+    print("Fork H2 failed")
+
+# else:
+##Procesos que realiza el PADRE
+# pid_list.append(pid)
+print("Father Waiting...")
+print(pid_H2)
+signal.signal(signal.SIGUSR1, handler_father)
+signal.signal(signal.SIGUSR2, handler_father)
+
+#Padre siempre esperando al principio
+while True:
+    signal.pause()
+    print("Father Reading: ", memory.readline().decode())
+    print("Father Notifying..")
 
 # for i in range(2):
 #     os.wait()
 
 # if __name__== "__main__":
 #     main()
+
+
+#Bibliografia:
+# De los os_ : https://www.tutorialspoint.com/python/os_open.htm
+# Del Double Fork: https://gist.github.com/Ma233/dd1f2f93db5378a29a3d1848288f520e
 
 
 #/home/aaron/Documents/Facultad/Tercer_Año/Computacion-II/TP6/
