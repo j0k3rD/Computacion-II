bc74879f102f8ec048ee162d77f9df15cc9713a8
Aaron <agm.moya@alumno.um.edu.ar>
2022-05-04 21:54:39 -0300

    tp6 fixed, add [:3]

--- TP6/mapping.py
+++ TP6/mapping.py
@@ -1,20 +1,13 @@
 #  Ejercicio del TP6 - Mapeado de Memoria y Sincronizacion - Leer y Almacenar
 
-# 
-
 import argparse
 import os
 import sys
 import mmap
 import signal
 
-# pid_H2 = 0
-
 
 def handler_father(s,f):
-    # if pid_H2 == 0:
-    #     pass
-    # else:
         if s == signal.SIGUSR1:
             os.kill(pid_H2, signal.SIGUSR1)
         elif s == signal.SIGUSR2:
@@ -35,9 +28,8 @@ def handler_H2(s,f):
         print("H2 Exiting")
         sys.exit(0)
 
-
-# def main(self):
 parser = argparse.ArgumentParser()
+#Pasar ubicacion, el archivo se crea solo
 parser.add_argument('-f',type=str,help="Ruta donde se ejecutara el archivo")
 args = parser.parse_args()
 
@@ -51,14 +43,10 @@ fd = os.open("{}test.txt".format(args.f), os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
 
 print("Starting..")
 
-# for i in range(2):
 try:
     pid = os.fork()
     if pid == 0:
-    # if not os.fork():
-        #Procesos que realiza el HIJO 1
-        # if i == 0:
-            # pid_list.append(os.getpid())
+        #Procesos que realiza el Hijo 1.
         for line in sys.stdin:
             if line[:3] == "bye":
                 os.kill(os.getppid(), signal.SIGUSR2)
@@ -66,17 +54,13 @@ try:
                 sys.exit(0)
             memory.write(line.encode())
             os.kill(os.getppid(),signal.SIGUSR1)
-        # pid_H2 = os.fork()
-        #         if not pid_H2:
 except OSError:
     print("Fork H1 failed")
 
 try:
-    # else:
         #Proceso que realiza el HIJO 2
     pid_H2 = os.fork() 
     if pid_H2 == 0:
-    # pid_H2 = os.getpid()
         signal.signal(signal.SIGUSR1, handler_H2)
         signal.signal(signal.SIGUSR2, handler_H2)
         while True:
@@ -84,30 +68,20 @@ try:
 except OSError:
     print("Fork H2 failed")
 
-# else:
 ##Procesos que realiza el PADRE
-# pid_list.append(pid)
 print("Father Waiting...")
 print(pid_H2)
 signal.signal(signal.SIGUSR1, handler_father)
 signal.signal(signal.SIGUSR2, handler_father)
 
-#Padre siempre esperando al principio
+#Padre siempre esperando a que le pase lineas el H1
 while True:
     signal.pause()
     print("Father Reading: ", memory.readline().decode())
     print("Father Notifying..")
-
-# for i in range(2):
-#     os.wait()
-
-# if __name__== "__main__":
-#     main()
 
 
 #Bibliografia:
 # De los os_ : https://www.tutorialspoint.com/python/os_open.htm
 # Del Double Fork: https://gist.github.com/Ma233/dd1f2f93db5378a29a3d1848288f520e
 
-
-#/home/aaron/Documents/Facultad/Tercer_Año/Computacion-II/TP6/
