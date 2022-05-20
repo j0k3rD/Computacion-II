e36448e74fbbf487ca0721c900e7af93417759aa
Aaron <agm.moya@alumno.um.edu.ar>
2022-05-20 17:38:41 -0300

    TODO: file lines

--- TP8/function_calculate.py
+++ TP8/function_calculate.py
@@ -26,73 +26,78 @@ import time
 import argparse
 import math
 
-class Main:
 
-    results = []
-
-    #Creacion de la funcion Main, donde se ejecutaran todos lo metodos del programa.
-    def main():
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-p',type=str,help="Enter the number of processes to create. EJ: '2', '5'")
-        parser.add_argument('-f',type=str,help="Enter the file path. EJ: '/home/user/Documents/test.txt.'")
-        parser.add_argument('-c',type=str,help="Select the function to calculate:   .root  .pot  .log")
-        args = parser.parse_args()
-
-
-        #Metodo que me muestra en que elemento estoy realizando el cálculo.
-        def sumador(x):
-            x = x+1
-
-        #Metodo que me trae cada una de las lineas del archivo
-        def fd_lines(args):
-            with open(args.l, "r") as fd:
-                line = fd.readline()
-                return line
-
-        #Metodo que calcula la raiz de c/u de los elementos del file
-        def root(x):
-            line = fd_lines()
-            print("Process %d calculing root of %d" %(os.getpid(), x))
-            res = math.sqrt(line)
-            return res
+results = []
+si = [1,2,3]
+    
+#Creacion de la funcion Main, donde se ejecutaran todos lo metodos del programa.
+# def main():
+parser = argparse.ArgumentParser()
+parser.add_argument('-p',type=str,help="Enter the number of processes to create. EJ: '2', '5'")
+parser.add_argument('-f',type=str,help="Enter the file path. EJ: '/home/user/Documents/test.txt.'")
+parser.add_argument('-c',type=str,help="Select the function to calculate:   .root  .pot  .log")
+args = parser.parse_args()
 
 
-        #Metodo que calcula la potencia de c/u los elementos del file
-        def pot(x):
-            line = fd_lines()
-            print("Proceso %d calculando cubo de %d" %(os.getpid(), x))
-            res = math.pow(line)
-            return res
+#Metodo que me muestra en que elemento estoy realizando el cálculo.
+def sumador(num):
+    num = num+1
+
+def pid(num):
+    return ("pid: %d - x=%d" % (os.getpid(), num))
+
+#Metodo que me trae cada una de las lineas del archivo
+def fd_lines(args):
+    with open(args.f) as fd:
+        for lineas in fd:
+            results.extend(lineas.split())
+        print(results)
+
+#Metodo que calcula la raiz de c/u de los elementos de los elementos de la linea de file
+def root(num):
+    print("Process %d calculing root of %d" %(os.getpid(), num))
+    return math.sqrt(num)
 
 
-        #Metodo que calcula el logaritmo de c/u de los elementos del file 
-        def log(x):
-            line = fd_lines()
-            print("Proceso %d calculando cubo de %d" %(os.getpid(), x))
-            res = math.log(line)
-            return res
+#Metodo que calcula la potencia de c/u de los elementos de los elementos de la linea de file
+def pot(num):
+    print("Process %d calculing root of %d" %(os.getpid(), num))
+    return math.pow(num,num)
 
 
-        #Creo mi pool con n procesos
-        pool = Pool(processes= int(args.p))
+#Metodo que calcula el logaritmo de c/u de los elementos de los elementos de la linea de file
+def log(num):
+    print("Process %d calculing root of %d" %(os.getpid(), num))
+    return math.log(num)
+
+#Creo mi pool con n procesos
+pool = Pool(processes= int(args.p))
 
 
-        #Con unos if decido que metodo aplicar para cada parametro en -c que se ingrese
-        if args.c == "root":
-            #Ejecuta la funcion root() con cada linea de la lista
-            results = pool.map(root, range(12))
-            print(results)
+#Con unos if decido que metodo aplicar para cada parametro en -c que se ingrese
+if args.c == "root":
+    #Ejecuta la funcion root() con cada linea de la lista
+    fd_lines(args)
+    print(si)
+    result = pool.map(root,si)
+    pool.close()
+    print(result)
 
-        elif args.c == "pot":
-            #Ejecuta la funcion pot() con cada linea de la lista
-            results = pool.map(pot, range(12))
-            print(results)
+elif args.c == "pot":
+    #Ejecuta la funcion pot() con cada linea de la lista
+    fd_lines(args)
+    print(si)
+    result = pool.map(pot,si)
+    pool.close()
+    print(result)
 
-        elif args.c == "log":
-            #Ejecuta la funcion log() con cada linea de la lista
-            results = pool.map(log, range(12))
-            print(results)
+elif args.c == "log":
+    #Ejecuta la funcion log() con cada linea de la lista
+    fd_lines(args)
+    print(si)
+    result = pool.map(log,si)
+    pool.close()
+    print(result)
 
-
-if __name__=='__main__':
-    Main.main()
+# if __name__=='__main__':
+#     main()
