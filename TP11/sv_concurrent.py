e067011ebe6d451138ca05dad52fcef3011bd44f
j0k3rD <agm.moya@alumno.um.edu.ar>
2022-09-12 11:45:56 -0300

    tp11 ended

--- TP11/sv_concurrent.py
+++ TP11/sv_concurrent.py
@@ -3,9 +3,9 @@ import concurrent.futures
 from subprocess import Popen, PIPE, STDOUT
 
 
-def handle(self):
+def handle(s2, addr):
     while True:
-        data = self.request.recv(1024).strip()
+        data = s2.recv(1024).strip()
         if data == b"exit":
             print("[~] Connection closed.")
             break
@@ -13,28 +13,28 @@ def handle(self):
             command = Popen({data}, stdout=PIPE, stderr=PIPE,shell=True)
             out, err = command.communicate()
             if out.decode() == "":
-                self.request.send(b"ERROR\n"+err)
+                s2.send(b"ERROR\n"+err)
             elif err.decode() == "" and len(out.decode()) > 1024:
                 max_size = len(out.decode())
-                self.request.send(b"OK\n[!](RESIZED=%i). Change 'Buffersize' to send all message.\n" %max_size+out[:1024])
+                s2.send(b"OK\n[!](RESIZED=%i). Change 'Buffersize' to send all message.\n" %max_size+out[:1024])
             elif err.decode() == "":
-                self.request.send(b"OK\n"+out)
+                s2.send(b"OK\n"+out)
 
-class ProcHandle():
+def ProcHandle(args):
     with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.bind(("", args.p))
+            s.bind(("", args[1]))
             s.listen(1)
             while True:
                 s2, addr = s.accept()
                 result = executor.submit(handle, s2, addr)
 
-class ThrHandle():
+def ThrHandle(args):
     with concurrent.futures.ThreadPoolExecutor(8) as executor:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.bind(("", args.p))
+            s.bind(args)
             s.listen(1)
             while True:
                 s2, addr = s.accept()
@@ -47,11 +47,13 @@ if __name__=="__main__":
     parser.add_argument('-c', type=str, help="Seleccionar 't' para threading y 'p' para process")
     args = parser.parse_args()
 
+    HOST, PORT = "", args.p
+
     if args.c == "t":
-        server = ThrHandle()
+        server = ThrHandle((HOST, PORT))
         server.serve_forever()
     elif  args.c == "p":
-        server = ProcHandle()
+        server = ProcHandle((HOST, PORT))
         server.serve_forever()
     else:
         print("[!] Error! Arguments not valid.")
